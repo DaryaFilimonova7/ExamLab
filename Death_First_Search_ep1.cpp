b498539d76c1300af9a1661b6a2ed2fd951fec4a
DaryaFilimonova7 <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-03-19 14:54:05 +0300

    Update Death_First_Search_ep1.cpp

--- Death_First_Search_ep1.cpp
+++ Death_First_Search_ep1.cpp
@@ -9,7 +9,6 @@ using namespace std;
 // Структура для хранения ребра 
 struct Edge {
     int source, destination;
-    bool is_gateway;
 };
 
 //ищет кратчайший путь от текущего положения вируса до гейта (не проверено, не доделано)
@@ -17,7 +16,7 @@ struct Edge {
 
 vector<int> BFS(int n, int si, int ei) {
     vector<int> distance(n,n);
-     vector<int> p(n, -1); // : у стартовой вершины предок — некоторая несуществующая вершина
+    vector<int> p(n, -1); // : у стартовой вершины предок — некоторая несуществующая вершина
     distance[si] = 0;
     queue<int> q;
     q.push(si);
@@ -28,7 +27,7 @@ vector<int> BFS(int n, int si, int ei) {
         int a = q.front();
         q.pop();
         // adjacent — список смежности
-        for (int b : adjacent[v]) {
+        for (int b : adjacent[a]) {
             if (distance[b] > distance[a] + 1) {
                 distance[b] = distance[a] + 1;
                 q.push(b);
@@ -77,7 +76,8 @@ int main()
         gateway[i] = ei;
     }
     
-    
+    /*
+    // добавим, если будет необходимо
     // помечаем ссылки, которые ведут к гейту
     for (int i = 0; i < n; i++) {
         for (j = 0; j < e; j++) {
@@ -86,6 +86,7 @@ int main()
                 eges[i].is_gateway = false;}
         }
     }
+    */
    
 
     // game loop
@@ -100,9 +101,12 @@ int main()
         // Example: 0 1 are the indices of the nodes you wish to sever the link between
         
         
-        // вызов ф-ии, пррсчитывающей кратчайший путь на каждое движение вируса
-        BFS(n,si,ei);
-
+        // вызов ф-ии, пррсчитывающей кратчайший путь до каждого из гейтов на каждое движение вируса
+        for (int i = 0; i < e; i++) {
+         BFS(n,si,gateway[i]);
+        }
+        
+      
         // далее делаем так, что блокируется путь от текущей точки в сторону кратчайшего пути
         // и удаляем этот путь из всех доступных нам, чтоб функция его больше не учитывала
         
