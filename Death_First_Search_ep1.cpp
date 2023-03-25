7d0a7032feac0793c7e9737500a675ea4fb0abbe
DaryaFilimonova7 <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-03-25 23:35:58 +0300

    Update Death_First_Search_ep1.cpp

--- Death_First_Search_ep1.cpp
+++ Death_First_Search_ep1.cpp
@@ -1,55 +1,23 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <list>
+
 
 using namespace std;
 
-// Структура для хранения ребра 
-struct Edge {
-    int source, destination;
-};
 
-//ищет кратчайший путь от текущего положения вируса до гейта (не проверено, не доделано)
-// источник -- https://algorithmica.org/tg/bfs
-
-vector<int> BFS(int n, int si, int ei) {
-    vector<int> distance(n,n);
-    vector<int> p(n, -1); // : у стартовой вершины предок — некоторая несуществующая вершина
-    distance[si] = 0;
-    queue<int> q;
-    q.push(si);
-    // Восстановление пути делается с конца. 
-    // Мы знаем последнюю вершину пути — это ei
-    // Далее, мы сводим задачу к меньшей, переходя к нахождению пути из ei в p(si)
-    while (!q.empty()) {
-        int a = q.front();
-        q.pop();
-        // adjacent — список смежности --- ЗАМЕНИТЬ НА СТРУКТУРУ?
-        for (int b : adjacent[a]) {
-            if (distance[b] > distance[a] + 1) {
-                distance[b] = distance[a] + 1;
-                q.push(b);
-            }
-        }
+void find_closest_gate(int e, vector<vector<int>> paths)
+{
+    int index = 0; // индекс пути к ближайшему гейту
+    int min = paths[0].size();
+    for (int j = 0; j < e; j++) {
+        if (paths[j].size() < min) { min = paths[j].size(); index = j; }
     }
-    // если пути не существует, возвращаем пустой vector
-    if (distance[ei] == n) {
-        return {};
-    }
-    vector<int> path;
-    while (ei != -1) {
-        path.push_back(ei);
-        ei = p[ei];
-    }
-    
-    // путь был рассмотрен в обратном порядке, поэтому его нужно перевернуть
-    reverse(path.begin(), path.end());
-    return path;
-
+    // блокируем ребро от текущего положения вируса до следующего в сторону короткого пути
+    cout << paths[index][0] << " " << paths[index][1] << endl;
 }
-
 
 
 int main()
@@ -59,65 +27,83 @@ int main()
     int e; // the number of exit gateways
     cin >> n >> l >> e; cin.ignore();
 
-    vector<Edge> edges;
+    // list<int> adj_list[n];  //create an array of lists whose size is n
+    vector<int> adj[n];
 
     for (int i = 0; i < l; i++) {
         int n1; // N1 and N2 defines a link between these nodes
         int n2;
-        // cin >> n1 >> n2; cin.ignore(); // способ ввода, который дан изначально
-        
-        cin >> edges[i].source >> edges[i].destination; cin.ignore();
+        cin >> n1 >> n2; cin.ignore();
+
+        adj[n1].push_back(n2);
+        adj[n2].push_back(n1);
     }
-    
+
     vector<int> gateway(e);
     for (int i = 0; i < e; i++) {
         int ei; // the index of a gateway node
         cin >> ei; cin.ignore();
         gateway[i] = ei;
     }
-    
-    /*
-    // добавим, если будет необходимо
-    // помечаем ссылки, которые ведут к гейту
-    for (int i = 0; i < n; i++) {
-        for (j = 0; j < e; j++) {
-                eges[i].is_gateway = true;}
-            else {
-                eges[i].is_gateway = false;}
-        }
-    }
-    */
-   
+
 
     // game loop
     while (1) {
         int si; // The index of the node on which the Bobnet agent is positioned this turn
         cin >> si; cin.ignore();
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+        vector<vector<int>> paths;
 
-
-        // Example: 0 1 are the indices of the nodes you wish to sever the link between
-        
-        
         // вызов ф-ии, пррсчитывающей кратчайший путь до каждого из гейтов на каждое движение вируса
         for (int i = 0; i < e; i++) {
-         BFS(n,si,gateway[i]);
-        }
-        
-      
-        // далее делаем так, что блокируется путь от текущей точки в сторону кратчайшего пути
-        // и удаляем этот путь из всех доступных нам, чтоб функция его больше не учитывала
-        
-        
-        /*
-            hardcode for test 2 
-            // cout << "0 2" << endl;
-            // cout << "1 3" << endl;
+            int visited[n] ={0};
+            int distance[n];
+            int parent[n];
+            visited[si] =1;
+            distance[si] =0;
+            parent[si]= -1;
+            queue<int> q;
+            q.push(si);
 
-        */
-        
+            while (!q.empty())
+            {
+                int v = q.front();
+                q.pop();
+
+                for(int u: adj[v]) {
+                    if (!visited[u]) {
+                        visited[u] =1;
+                        q.push(u);
+                        distance[u] = distance[v]+1;  // coming from v to u
+                        parent[u] = v;
+                    }
+                }
+            }
+
+            /*
+            for (int j =0; j<n; j++) {
+                cout << distance[j] << " ";  кратчайший путь до каждой вершины
+            }
+             */
+
+            // path to destination
+            int x = gateway[i];
+            vector<int> path;
+            while (x != -1) {
+                path.push_back(x);
+                x = parent[x];
+            }
+            reverse(path.begin(), path.end());
+            paths.push_back(path);
+
+        }
+
+        // далее делаем так, что блокируется путь от текущей точки в сторону кратчайшего пути
+        find_closest_gate(e, paths);
+
+
+        // и удаляем этот путь из всех доступных нам, чтоб функция его больше не учитывала
+
     }
-    
+
 }
