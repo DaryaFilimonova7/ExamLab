bfb4ce02b73d42b7535cb888f7d15b6c8d405754
DaryaFilimonova7 <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-03-25 15:10:08 +0300

    Update another_solvation.cpp

--- another_solvation.cpp
+++ another_solvation.cpp
@@ -11,23 +11,13 @@ using namespace std;
 const int imax = std::numeric_limits<int>::max();  //imax declared as the max value for int (in C++)
 
 
-void add_edge(vector<vector<int>> adj, int a, int b) {    //add a into the list b, and b into list a
-   adj[a][b] = 1;
-   adj[b][a] = 1;
-}
-
-// только нам надо сделать не принт весь путь, а только два узла
-void printPath(int s, int t, const vector<int> &path)
-{
-    if (t == s) {
-        return;
-    }
-    printPath(s, path[t], path);
-    cout << path[t];
+void add_edge(vector<vector<int>> adj, int a, int b) {
+    adj[a][b] = 1;
+    adj[b][a] = 1;
 }
 
 
-void dfs(int n, vector<vector<int>> adj, int si, int ei)
+void dfs(int n, vector<vector<int>> adj, int si, int ei, vector<vector<int>> paths)
 {
     vector<int> distance(n, imax);
     vector<int> path;
@@ -44,12 +34,22 @@ void dfs(int n, vector<vector<int>> adj, int si, int ei)
             }
         }
     }
-     /* t can be anything you want */
+    /* t can be anything you want */
     int t = 5;
-    printPath(si, t, path); cout << endl;                                                // не понимаю эту часть - что такое t 
+    paths.push_back(path);
+                                                                                        // не понимаю эту часть - что такое t
 }                                                                                       // может быть t это ei ? типа конец пути
 
-
+void find_closest_gate(int e, vector<vector<int>> paths)
+{
+    int index = 0; // индекс пути к ближайшему гейту
+    int min = paths[0].size();
+    for (int j = 0; j < e; j++) {
+        if (paths[j].size() < min) { min = paths[j].size(); index = j; }
+    }
+    // блокируем ребро от текущего положения вируса до следующего в сторону короткого пути
+    cout << paths[index][0] << " " << paths[index][1] << endl;
+}
 
 
 int main()
@@ -60,42 +60,45 @@ int main()
     cin >> n >> l >> e; cin.ignore();
 
     // list<int> adj_list[n];  //create an array of lists whose size is n
-    vector<vector<int>> adj(n); 
+    vector<vector<int>> adj(n);
 
     for (int i = 0; i < l; i++) {
         int n1; // N1 and N2 defines a link between these nodes
         int n2;
-        cin >> n1 >> n2; cin.ignore(); 
-    
+        cin >> n1 >> n2; cin.ignore();
+
         add_edge(adj, n1, n2);
     }
-    
+
     vector<int> gateway(e);
     for (int i = 0; i < e; i++) {
         int ei; // the index of a gateway node
         cin >> ei; cin.ignore();
         gateway[i] = ei;
     }
-   
+
 
     // game loop
     while (1) {
         int si; // The index of the node on which the Bobnet agent is positioned this turn
         cin >> si; cin.ignore();
 
+        vector<vector<int>> paths;
+
         // вызов ф-ии, пррсчитывающей кратчайший путь до каждого из гейтов на каждое движение вируса
         for (int i = 0; i < e; i++) {
-            dfs(n, adj, si, gateway[i]);
+            dfs(n, adj, si, gateway[i], paths);
         }
-       
-       // нужно добавлять созданные до разных гейтов пути в вектор, потом найти путь с минимальным 
-       // кол-вом элементов, оттуда выделить два узла, которые необходимо напечатать
         
-      
+        
         // далее делаем так, что блокируется путь от текущей точки в сторону кратчайшего пути
+        if (e != 1) {
+            find_closest_gate(e, paths);
+        }
+        
+        
         // и удаляем этот путь из всех доступных нам, чтоб функция его больше не учитывала
-        
-        
+
     }
-    
+
 }
