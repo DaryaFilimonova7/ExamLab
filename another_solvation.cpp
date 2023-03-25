e06e892110031a67444e3067b5aaf7072851c2e4
DaryaFilimonova7 <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-03-25 13:33:32 +0300

    Update another_solvation.cpp

--- another_solvation.cpp
+++ another_solvation.cpp
@@ -4,38 +4,17 @@
 #include <algorithm>
 #include <queue>
 #include <limits>
+#include <list>
 
 
 using namespace std;
 const int imax = std::numeric_limits<int>::max();  //imax declared as the max value for int (in C++)
 
 
-void add_edge(list<int> adj_list[], int a, int b) {    //add a into the list b, and b into list a
-   adj_list[a].push_back(b);
-   adj_list[b].push_back(a);
+void add_edge(vector<vector<int>> adj, int a, int b) {    //add a into the list b, and b into list a
+   adj[a][b] = 1;
+   adj[b][a] = 1;
 }
-
-void dfs(int n, vector<vector<int>> adjList, int si, int ei)
-{
-    vector<int> distance(n, imax);
-    vector<int> path;
-    queue<int> q; q.push(si); distance[si] = 0;
-
-    while (!q.empty()) {
-        auto curr = q.front(); q.pop();
-
-        for (int i = 0; i < (int)adjList[curr].size(); ++i) {
-            if (distance[i] == imax) {
-                distance[i] = distance[curr] + 1;
-                //save the parent to have the path at the end of the algo.
-                path[i] = curr;
-            }
-        }
-    }
-     /* t can be anything you want */
-    int t = 5;
-    printPath(s, t, path); cout << endl;                                                // не понимаю эту часть - что такое t 
-}                                                                                       // может быть t это ei ? типа конец пути
 
 // только нам надо сделать не принт весь путь, а только два узла
 void printPath(int s, int t, const vector<int> &path)
@@ -48,6 +27,30 @@ void printPath(int s, int t, const vector<int> &path)
 }
 
 
+void dfs(int n, vector<vector<int>> adj, int si, int ei)
+{
+    vector<int> distance(n, imax);
+    vector<int> path;
+    queue<int> q; q.push(si); distance[si] = 0;
+
+    while (!q.empty()) {
+        auto curr = q.front(); q.pop();
+
+        for (int i = 0; i < (int)adj[curr].size(); ++i) {
+            if (distance[i] == imax) {
+                distance[i] = distance[curr] + 1;
+                //save the parent to have the path at the end of the algo.
+                path[i] = curr;
+            }
+        }
+    }
+     /* t can be anything you want */
+    int t = 5;
+    printPath(si, t, path); cout << endl;                                                // не понимаю эту часть - что такое t 
+}                                                                                       // может быть t это ei ? типа конец пути
+
+
+
 
 int main()
 {
@@ -56,14 +59,15 @@ int main()
     int e; // the number of exit gateways
     cin >> n >> l >> e; cin.ignore();
 
-    list<int> adj_list[n];  //create an array of lists whose size is n
+    // list<int> adj_list[n];  //create an array of lists whose size is n
+    vector<vector<int>> adj(n); 
 
     for (int i = 0; i < l; i++) {
         int n1; // N1 and N2 defines a link between these nodes
         int n2;
         cin >> n1 >> n2; cin.ignore(); 
     
-        add_edge(adj_list, n1, n2);
+        add_edge(adj, n1, n2);
     }
     
     vector<int> gateway(e);
@@ -81,7 +85,7 @@ int main()
 
         // вызов ф-ии, пррсчитывающей кратчайший путь до каждого из гейтов на каждое движение вируса
         for (int i = 0; i < e; i++) {
-          dfs(n, adjList, si, gateway[i])
+            dfs(n, adj, si, gateway[i]);
         }
         
       
